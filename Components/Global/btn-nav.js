a143a6dfc2e818d7f7ce729e076b4479d948e7f7
Dashka_W <Daskendrik@users.noreply.github.com>
2023-09-05 13:59:16 +0300

    delete react

---
+++ Components/Global/btn-nav.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+function ButtonNav(prop) {}
+
+const buttonNav = <span className="btn">{name}</span>;
+
+const container = document.getElementById('header-root');
+const root = ReactDOM.createRoot(container);
+root.render(buttonNav);
--- Components/Global/header.js
+++ Components/Global/header.js
@@ -1,3 +1,11 @@
 'use strict';
 
-function Header() {}
+const Header = () => {
+  let [testText, setTestText] = React.useState('Click');
+  const test = () => setTestText('Cliiiiiiiiiiiiiiiiiiiiiiiiick');
+  return <button onClick={test}>{testText}</button>;
+};
+
+const container = document.getElementById('header-root');
+const root = ReactDOM.createRoot(container);
+root.render(<Header />);
