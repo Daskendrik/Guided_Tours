29b969ecab6adc27ed38e33caceb518c37be4cd1
Dasha <52924734+Daskendrik@users.noreply.github.com>
2023-09-29 16:38:21 +0300

    -/-

--- react/tourreact/src/Components/Pages/Museum.js
+++ react/tourreact/src/Components/Pages/Museum.js
@@ -1,11 +1,11 @@
-import { UIForm } from '../../Data/museums';
+import { UIForm, UIFormData } from '../../Data/museums';
 import FormApplet from '../FormApplets/FormApplet';
 import ListApplet from '../ListApplets/ListApplet';
 
 const Museum = () => {
   return (
     <>
-      <FormApplet UIForm={UIForm} />
+      <FormApplet UIForm={UIForm} UIFormData={UIFormData} />
       <ListApplet />
     </>
   );
--- react/tourreact/src/Data/museums.js
+++ react/tourreact/src/Data/museums.js
@@ -3,10 +3,21 @@ const UIForm = [
   'Город',
   'Адрес',
   'Телефон',
-  'Парковка',
+  'Стоимость',
   'Выходной',
+  'Коментарий',
 ];
+
+const UIFormData = {
+  Name: ['Коломенское', 'text'],
+  City: ['Москва', 'text'],
+  Adress: ['Адрес', 'textarea'],
+  Phone: ['Телефон', 'tel'],
+  Cost: ['Стоимость', 'textarea'],
+  Weekend: ['Выходной', 'text'],
+  Comments: ['Коментарий', 'textarea'],
+};
 const UIFormMap = ['Адрес'];
 const UIListUIWorkTime = ['День', 'Время работы'];
 
-export { UIForm, UIFormMap, UIListUIWorkTime };
+export { UIForm, UIFormMap, UIListUIWorkTime, UIFormData };
