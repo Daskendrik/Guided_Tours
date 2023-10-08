1c7710109ee37f8c0e0e0a195bd735f94375ba27
Daskendrik <ss599@yandex.ru>
2023-10-08 12:40:53 +0300

    add columne cases

--- react/tourreact/src/Components/FormApplets/FormColum.js
+++ react/tourreact/src/Components/FormApplets/FormColum.js
@@ -3,7 +3,10 @@ const FormColum = (props) => {
   switch (type) {
     case 'textarea':
       return <textarea defaultValue={velue}></textarea>;
-
+    case 'tel':
+      return <input defaultValue={velue} type="tel"></input>;
+    case 'email':
+      return <input defaultValue={velue} type="email"></input>;
     default:
       return <input defaultValue={velue}></input>;
   }
--- react/tourreact/src/Components/Pages/Restaurant.js
+++ react/tourreact/src/Components/Pages/Restaurant.js
@@ -1,10 +1,57 @@
+import { useState } from 'react';
 import FormApplet from '../FormApplets/FormApplet';
 import ListApplet from '../ListApplets/ListApplet';
+import { v4 as uuidv4 } from 'uuid';
 
 const Restaurant = () => {
+  const [arrColum, setArrColum] = useState([
+    {
+      Lable: 'Название',
+      Velue: 'Заглушка',
+      Type: 'text',
+      id: uuidv4(),
+    },
+    {
+      Lable: 'Город',
+      Velue: 'Заглушка',
+      Type: 'text',
+      id: uuidv4(),
+    },
+    {
+      Lable: 'Адрес',
+      Velue: 'Заглушка',
+      Type: 'textarea',
+      id: uuidv4(),
+    },
+    {
+      Lable: 'Сайт',
+      Velue: 'Заглушка',
+      Type: 'text',
+      id: uuidv4(),
+    },
+    {
+      Lable: 'Телефон',
+      Velue: 'Заглушка',
+      Type: 'tel',
+      id: uuidv4(),
+    },
+    {
+      Lable: 'Коментарий',
+      Velue: 'Тут много чего будет написано',
+      Type: 'textarea',
+      id: uuidv4(),
+    },
+    {
+      Lable: 'Почта',
+      Velue: 'Заглушка',
+      Type: 'email',
+      id: uuidv4(),
+    },
+  ]);
+
   return (
     <>
-      <FormApplet />
+      <FormApplet arrColum={arrColum} title="Информация" />
       <ListApplet />
     </>
   );
