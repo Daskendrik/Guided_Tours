8576924ef915f2bc02ed0d9a6c8cb2c35dc8d4d3
Dashka_W <Daskendrik@users.noreply.github.com>
2023-10-13 19:03:31 +0300

    -/-

--- react/tourreact/src/Components/Pages/Applets/MainFormAppletGuide.jsx
+++ react/tourreact/src/Components/Pages/Applets/MainFormAppletGuide.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import FormApplet from '../../FormApplets/FormApplet';
 const MainFormAppletGuide = () => {
@@ -8,6 +8,31 @@ const MainFormAppletGuide = () => {
   const [telInfo, setTelInfo] = useState('Test');
   const [documentInfo, setDocumentInfo] = useState('Test');
   const [commetInfo, setCommetInfo] = useState('Test');
+  let data = {};
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch(
+          'https://my-json-server.typicode.com/Daskendrik/demo_data/db'
+        );
+        data = await res.json();
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+      console.log('finish');
+    }
+    fetchData();
+  }, []);
+  if (!!data) {
+    setNameInfo(data.nameInfo);
+    setCityInfo(data.cityInfo);
+    setAdressInfo(data.adressInfo);
+    setTelInfo(data.telInfo);
+    setDocumentInfo(data.documentInfo);
+    setCommetInfo(data.nameInfo);
+  }
+
   return <FormApplet />;
 };
 
