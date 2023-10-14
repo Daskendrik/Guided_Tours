e6c156526fa48e748624b8fea2135b4e300eb051
Dasha <52924734+Daskendrik@users.noreply.github.com>
2023-10-14 20:07:35 +0300

    make footer

--- react/tourreact/src/App.js
+++ react/tourreact/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Guide from './Components/Pages/Views/Guide';
+import Footer from './Components/Additional/Footer';
 
 function App() {
   return (
     <div className="App">
       <h3>Пока что без меню, когда нить тут оно будет</h3>
       <Guide />
+      <Footer />
     </div>
   );
 }
