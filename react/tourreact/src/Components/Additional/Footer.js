b0ee7c8975f0f246ddf5e40213698da6a9dae5cc
Dasha <52924734+Daskendrik@users.noreply.github.com>
2023-10-14 20:12:32 +0300

    css

--- react/tourreact/src/Components/Additional/Footer.js
+++ react/tourreact/src/Components/Additional/Footer.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import styles from './Footer.module.css';
 
 const Footer = () => {
-  return <div>Тут будет футер</div>;
+  return <div className={styles.footer}>Тут будет футер</div>;
 };
 
 export default Footer;
