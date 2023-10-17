47ef007ffb0b69c71c3167785af954647886b50c
Dashka_W <Daskendrik@users.noreply.github.com>
2023-10-17 15:53:09 +0300

    new test

--- react/tourreact/src/App.js
+++ react/tourreact/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Guide from './Components/Pages/Views/Guide';
 import Footer from './Components/Additional/Footer';
+import Museum from './Components/Pages/Views/Museum';
 
 function App() {
   return (
     <div className="App">
       <h3>Пока что без меню, когда нить тут оно будет</h3>
-      <Guide />
+      <Museum />
       <Footer />
     </div>
   );
