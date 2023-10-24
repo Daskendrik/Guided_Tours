9451055b34f3b9281da3334dd84c69ea88ac4a4a
Dashka_W <Daskendrik@users.noreply.github.com>
2023-10-24 17:51:46 +0300

    test

--- back/main.go
+++ back/main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	message := "World hold on1"
+	var message2 string 
+	message2 = "World hold on2"
 	fmt.Println("World hold on")
+	fmt.Println(message)
+	fmt.Println(message2)
 }
