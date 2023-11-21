d3370c59a60bef88ce694c6587373c3df579ab3e
Dashka_W <Daskendrik@users.noreply.github.com>
2023-11-21 17:48:52 +0300

    begin buses

---
+++ routes/buses.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/bus', (req, res) => {
+  res.status(200).json({
+    list: [
+      ['1234', '123', '123'],
+      ['12345', '123', '123'],
+      ['123567', '123', '123'],
+    ],
+  });
+});
+
+module.exports = router;
