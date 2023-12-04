ca95f41cc89ba2b311e054ee94e9f126fe2f9c92
Daskendrik <ss599@yandex.ru>
2023-12-04 15:49:52 +0300

    настроила и подключила к локалке

--- src/Components/Pages/Lists/ListOfCompanyBus.js
+++ src/Components/Pages/Lists/ListOfCompanyBus.js
@@ -1,31 +1,73 @@
+import { useEffect, useState } from 'react';
 import ListApplet from '../../ListApplets/ListApplet';
+import ErrorServer from '../../Additional/ErrorServer';
+import Loading from '../../Additional/Loading';
+import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
 
 const ListOfCompanyBus = () => {
-  let dataList = [];
-  dataList = [
+  const [textError, setTextError] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const [dataList, setDataList] = useState([]);
+  const [targetRow, setTargetRow] = useState('');
+  const buttons = [
     {
-      element: 'Header',
-      nameColumn: [
-        { title: 'Название', id: 'name' },
-        { title: 'Телефон фирмы', id: 'firmTel' },
-      ],
+      title: 'Создать новую запись',
+      func: function handleCreateTC() {
+        console.log('Заглущка');
+      },
+      id: uuidv4(),
     },
     {
-      element: 'Body',
-      elements: [
-        ['1234', '123'],
-        ['12345', '123'],
-        ['123567', '123'],
-        ['123567', '123'],
-        ['123567', '123'],
-        ['123567', '123'],
-        ['123567', '123'],
-        ['123567', '123'],
-      ],
+      title: 'Удалить запись',
+      func: function handleCreateTC() {
+        console.log('Заглущка');
+      },
+      id: uuidv4(),
     },
   ];
+  const handleSetTargetRow = (e) => {
+    setTargetRow(e.target.parentElement.id);
+    console.log(targetRow);
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch(
+          'http://localhost:3001/api/company_bus/getAllCompanyBus'
+        );
+        const dataIntegration = await res.json();
+        console.log(dataIntegration);
+        if (!!dataIntegration) {
+          setDataList(dataIntegration.req);
+          console.log(dataIntegration.req);
+        }
+      } catch (error) {
+        console.log(error);
+        setTextError(error.message);
+      }
+      console.log('finish');
+      setIsLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  if (textError) {
+    return <ErrorServer textError={textError} />;
+  }
+  console.log(isLoading);
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
-    <ListApplet title="Список всех транспортных фирм" arrListColum={dataList} />
+    <ListApplet
+      title="Список всех транспортных фирм"
+      arrListColum={dataList}
+      buttons={buttons}
+      changeTarget={handleSetTargetRow}
+    />
   );
 };
 
