5ece8702a9ebb67ce85c7c0f8c40f856bd7ad89b
Daskendrik <ss599@yandex.ru>
2024-01-29 15:28:50 +0300

    поправила

--- src/Components/ListApplets/ListApplet.js
+++ src/Components/ListApplets/ListApplet.js
@@ -8,23 +8,7 @@ import { useState } from 'react';
 const ListApplet = (props) => {
   const {
     buttons = [{ title: 'Кнопка', function: '', id: uuidv4(), link: '' }],
-    arrListColum = [
-      {
-        element: 'Header',
-        nameColumn: [
-          { title: 'Test', id: 'number' },
-          { title: 'Test', id: 'name' },
-        ],
-      },
-      {
-        element: 'Body',
-        elements: [
-          ['1234', '123'],
-          ['12345', '123'],
-          ['123567', '123'],
-        ],
-      },
-    ],
+    arrListColum = [],
     title = 'Test',
     changeTarget,
     targetRow,
@@ -41,7 +25,8 @@ const ListApplet = (props) => {
   }
 
   const goNextPage = () => {
-    if (page < blockDatas.length) {
+    if (page < blockDatas.length - 1) {
+      console.log('page');
       let a = page;
       setPage(a + 1);
     }
@@ -53,86 +38,129 @@ const ListApplet = (props) => {
       setPage(a - 1);
     }
   };
-
-  return (
-    <>
-      <div className={styles.list_applet}>
-        <div className={styles.header}>
-          <div className={styles.title}>{title}</div>
-          <div className={styles.button}>
-            {buttons.map((btn) => {
-              return (
-                <StandartBtn
-                  key={btn.id}
-                  title={btn.title}
-                  func={btn.func}
-                  link={btn.link}
-                />
-              );
-            })}
+  if (blockDatas.length > 0) {
+    return (
+      <>
+        <div className={styles.list_applet}>
+          <div className={styles.header}>
+            <div className={styles.title}>{title}</div>
+            <div className={styles.button}>
+              {buttons.map((btn) => {
+                return (
+                  <StandartBtn
+                    key={btn.id}
+                    title={btn.title}
+                    func={btn.func}
+                    link={btn.link}
+                  />
+                );
+              })}
+            </div>
           </div>
-        </div>
-        <div className={styles.list_applet_table}>
-          <div>
-            <table>
-              <thead>
-                <tr>
-                  {header.nameColumn.map((col) => {
-                    return <td key={col.id}>{col.title}</td>;
-                  })}
-                </tr>
-              </thead>
-              <tbody>
-                {blockDatas[page].map((row, index) => {
-                  return (
-                    <tr key={row[0]} id={row[0]} onClick={changeTarget}>
-                      {row.map((col, index) => {
-                        if (index === 1) {
-                          return (
-                            <td
-                              key={index}
-                              className={
-                                +targetRow === row[0] ? 'active_row' : ''
-                              }
-                            >
-                              <Link
-                                className={styles.go_in}
-                                to={row[0].toString()}
+          <div className={styles.list_applet_table}>
+            <div>
+              <table>
+                <thead>
+                  <tr>
+                    {header.nameColumn.map((col) => {
+                      return <td key={col.id}>{col.title}</td>;
+                    })}
+                  </tr>
+                </thead>
+                <tbody>
+                  {blockDatas[page].map((row, index) => {
+                    return (
+                      <tr key={row[0]} id={row[0]} onClick={changeTarget}>
+                        {row.map((col, index) => {
+                          if (index === 1) {
+                            return (
+                              <td
+                                key={index}
+                                className={
+                                  +targetRow === row[0] ? 'active_row' : ''
+                                }
+                              >
+                                <Link
+                                  className={styles.go_in}
+                                  to={row[0].toString()}
+                                >
+                                  {col}
+                                </Link>
+                              </td>
+                            );
+                          } else {
+                            return (
+                              <td
+                                key={index}
+                                className={
+                                  +targetRow === row[0] ? 'active_row' : ''
+                                }
                               >
                                 {col}
-                              </Link>
-                            </td>
-                          );
-                        } else {
-                          return (
-                            <td
-                              key={index}
-                              className={
-                                +targetRow === row[0] ? 'active_row' : ''
-                              }
-                            >
-                              {col}
-                            </td>
-                          );
-                        }
-                      })}
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
-          <div>
-            <ListOfButtonsPage
-              pageNext={goNextPage}
-              pageBack={goBackPage}
-              page={page + 1}
-            />
+                              </td>
+                            );
+                          }
+                        })}
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+            <div>
+              <ListOfButtonsPage
+                pageNext={goNextPage}
+                pageBack={goBackPage}
+                page={page + 1}
+              />
+            </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className={styles.list_applet}>
+          <div className={styles.header}>
+            <div className={styles.title}>{title}</div>
+            <div className={styles.button}>
+              {buttons.map((btn) => {
+                return (
+                  <StandartBtn
+                    key={btn.id}
+                    title={btn.title}
+                    func={btn.func}
+                    link={btn.link}
+                  />
+                );
+              })}
+            </div>
+          </div>
+          <div className={styles.list_applet_table}>
+            <div>
+              <table>
+                <thead>
+                  <tr>
+                    {header.nameColumn.map((col) => {
+                      return <td key={col.id}>{col.title}</td>;
+                    })}
+                  </tr>
+                </thead>
+              </table>
+            </div>
+            <div>
+              <ListOfButtonsPage
+                pageNext={goNextPage}
+                pageBack={goBackPage}
+                page={page + 1}
+              />
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
 };
 
 export default ListApplet;
