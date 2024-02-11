cbf10f41b0bd041fee49601a42a477d734fa9863
Daskendrik <ss599@yandex.ru>
2024-02-11 14:51:46 +0300

    переделала структуру

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import ListOfGuide from './Components/Pages/Lists/ListOfGuide';
 import ListOfRestorant from './Components/Pages/Lists/ListOfRestorant';
 import ListOfTour from './Components/Pages/Lists/ListOfTour';
 import ListOfTypeLOV from './Components/Pages/Lists/ListOfTypeLOV';
-import ListOfContact from './Components/Pages/Lists/ListOfContact';
+import ListOfContacts from './Components/Pages/Views/Contact/ListOfContacts';
 import Contact from './Components/Pages/Views/Contact/Contact';
 import ContactNew from './Components/Pages/Views/Contact/ContactNew';
 import ContactEdit from './Components/Pages/Views/Contact/ContactEdit';
@@ -32,7 +32,7 @@ function App() {
               <Route path="tour" element={<ListOfTour />} />
               <Route path="сalendar" element={<Calendar />} />
               <Route path="type_lov" element={<ListOfTypeLOV />} />
-              <Route path="contact" element={<ListOfContact />} />
+              <Route path="contact" element={<ListOfContacts />} />
               <Route path="contact/:id" element={<Contact />} />
               <Route path="contact/new" element={<ContactNew />} />
               <Route path="contact/edit/:id" element={<ContactEdit />} />
---
+++ src/Components/Pages/Views/Contact/Types.ts
@@ -0,0 +1,9 @@
+export type Contact = {
+  id: number;
+  name: string;
+  middleName: string;
+  lastName: string;
+  tel: number;
+  fullName: string;
+  type: string;
+};
